9ad360b80fb786dafd9bbeb7d965b758a2bd6608
Blake Borgna <blakeborgna@hotmail.com>
2020-10-18 16:17:16 +1000

    Add robot command handling and tests

--- src/lib/robot.js
+++ src/lib/robot.js
@@ -1,3 +1,6 @@
+// While lower camel case would normally be used for a
+// variable, upper camel case is used here as it will
+// be used as an enum.
 export const Direction = {
     NORTH: 0,
     EAST: 90,
@@ -6,35 +9,132 @@ export const Direction = {
 }
 Object.freeze(Direction);
 
+const directionDescriptions = {
+    0: "NORTH",
+    90: "EAST",
+    180: "SOUTH",
+    270: "WEST",
+}
+
 export class Robot{
-    constructor(x, y, facing) {
-        this.x = x;
-        this.y = y;
-        this.facing = Direction[facing];
+    constructor() {
+        this.isPlaced = false;
     }
 
-    static placeRobot(x, y, facing) {
+    place(x, y, facing) {
         // This implementation assumes only integer positions are valid
-        const xIsValid = Number.isInteger(x) && 0 <= x && x <= 4;
-        const yIsValid = Number.isInteger(y) && 0 <= y && y <= 4;
+        const xNum = parseFloat(x);
+        const yNum = parseFloat(y);
+        const xIsValid = Number.isInteger(xNum) && 0 <= xNum && xNum <= 4;
+        const yIsValid = Number.isInteger(yNum) && 0 <= yNum && yNum <= 4;
         const facingIsValid = Direction.hasOwnProperty(facing);
-        
+
         if ( xIsValid && yIsValid && facingIsValid) {
             // The starting position is valid
-            return new Robot(x, y, facing);
+            this.isPlaced = true;
+            this.x = xNum;
+            this.y = yNum;
+            this.facing = Direction[facing];
         }
     }
 
     move() {
-        
+        const invalidMoveEast = this.x === 4 && this.facing == Direction.EAST;
+        const invalidMoveWest = this.x === 0 && this.facing == Direction.WEST;
+        const invalidMoveSouth = this.y === 0 && this.facing == Direction.SOUTH;
+        const invalidMoveNorth = this.y === 4 && this.facing == Direction.NORTH;
+        const invalidMove = invalidMoveEast || invalidMoveWest || invalidMoveSouth || invalidMoveNorth;
+        if (invalidMove) {
+            return;
+        }
+        // If we get to this point we know the robot is allowed to move
+        switch (this.facing) {
+            case Direction.NORTH:
+                this.y += 1;
+                break;
+            case Direction.SOUTH:
+                this.y -= 1;
+                break;
+            case Direction.EAST:
+                this.x += 1;
+                break;
+            case Direction.WEST:
+                this.x -= 1;
+                break;
+            default:
+                throw Error("No direction to move in");
+        }
+    }
+
+    rotateRight() {
+        if (this.facing === undefined) {
+            throw Error("No direction to start rotation")
+        }
+        this.facing = (this.facing + 90) % 360;
+    }
+
+    rotateLeft() {
+        // Subtract 90 to perform the rotation, 
+        // then add 360 to shift the value by 
+        // a full rotation so all values are positive,
+        // then mod 360 to get the simplified offset.
+        if (this.facing === undefined) {
+            throw Error("No direction to start rotation")
+        }
+        this.facing = (this.facing - 90 + 360) % 360;
+    }
+
+    report() {
+        if (this.facing === undefined) {
+            throw Error("No direction to report");
+        }
+        const direction = directionDescriptions[this.facing];
+        return `${this.x},${this.y},${direction}`;
     }
 
     followCommand(command) {
-        const commandParts = command.split(" ", 2);
-        switch(commandParts[0]) {
-            case "place":
-                break;
-                
+        let action;
+        let parameters = "";
+        let report;
+        let error;
+        
+        const commandUpper = command.toUpperCase();
+        const separatorIndex = commandUpper.indexOf(" ");
+        if (separatorIndex === -1) {
+            action = commandUpper;
+        } else {
+            action = commandUpper.slice(0, separatorIndex);
+            parameters = commandUpper.slice(separatorIndex + 1).replace(" ", "").split(",");
         }
+        
+        if (action === "PLACE") {
+            if (parameters.length !== 3) {
+                error = "Three parameters required when placing";
+            } else {
+                this.place(parameters[0].trim(), parameters[1].trim(), parameters[2].trim());
+            }
+        } else if (this.isPlaced) {
+            if (parameters !== "") {
+                error = "Malformed command"
+            } else {
+                switch(action) {
+                    case "MOVE":
+                        this.move();
+                        break;
+                    case "RIGHT":
+                        this.rotateRight();
+                        break;
+                    case "LEFT":
+                        this.rotateLeft();
+                        break;
+                    case "REPORT":
+                        report = this.report();
+                        break;
+                    default:
+                        error = "Unknown command";
+                }
+            }
+        }
+        return { report, error };        
     }
 }
--- src/lib/robot.test.js
+++ src/lib/robot.test.js
@@ -1,69 +1,495 @@
 import { Direction, Robot } from './robot';
 
-test("can initialise robot with parameters", () => {
-    // We only test with valid inputs here because 
-    // it is assumed the constructor will only be
-    // directly called with verified input.
-    // Unverified input will be handled by the static
-    // placeRobot method.
-    const robotN = new Robot(0, 0, "NORTH");
-    expect(robotN.x).toBe(0);
-    expect(robotN.y).toBe(0);
-    expect(robotN.facing).toBe(Direction.NORTH);
+/**
+ * Helper function to check the robot state is correct.
+ * @param {} robot Robot to be verified.
+ * @param {*} expectedX Desired x value of the robot.
+ * @param {*} expectedY Desired y value of the robot.
+ * @param {*} expectedFacing Desired facing value of the robot.
+ */
+function checkRobotValues(robot, expectedIsPlaced, expectedX, expectedY, expectedFacing, ) {
+    expect(robot.isPlaced).toBe(expectedIsPlaced);
+    expect(robot.x).toBe(expectedX);
+    expect(robot.y).toBe(expectedY);
+    expect(robot.facing).toBe(expectedFacing);
+}
 
-    const robotS = new Robot(1, 4, "SOUTH");
-    expect(robotS.x).toBe(1);
-    expect(robotS.y).toBe(4);
-    expect(robotS.facing).toBe(Direction.SOUTH);
-
-    const robotE = new Robot(2, 2, "EAST");
-    expect(robotE.x).toBe(2);
-    expect(robotE.y).toBe(2);
-    expect(robotE.facing).toBe(Direction.EAST);
-
-    const robotW = new Robot(3, 4, "WEST");
-    expect(robotW.x).toBe(3);
-    expect(robotW.y).toBe(4);
-    expect(robotW.facing).toBe(Direction.WEST);
+test("can initialise robot", () => {
+    const robot = new Robot();
+    expect(robot.isPlaced).toBeFalsy();
 });
 
 test("cannot place robot with non-integer position", () => {
     // It has been assumed that only integer positions are allowed.
-    expect(Robot.placeRobot(0.3, 3, "NORTH")).toBe(undefined);
-    expect(Robot.placeRobot(3, 0.1, "SOUTH")).toBe(undefined);
-    expect(Robot.placeRobot("x pos", 3, "EAST")).toBe(undefined);
-    expect(Robot.placeRobot(3, "y pos", "WEST")).toBe(undefined);
+    const robot = new Robot();
+    robot.place(0.3, 3, "NORTH");
+    checkRobotValues(robot, false);
+
+    robot.place(3, 0.1, "SOUTH");
+    checkRobotValues(robot, false);
+
+    robot.place("x pos", 3, "EAST");
+    checkRobotValues(robot, false);
+
+    robot.place(3, "y pos", "WEST");
+    checkRobotValues(robot, false);
 });
 
 test("cannot place robot outside bounds", () => {
-    expect(Robot.placeRobot(-1, 0, "NORTH")).toBe(undefined)
-    expect(Robot.placeRobot(1, -3, "EAST")).toBe(undefined)
-    expect(Robot.placeRobot(5, 0, "NORTH")).toBe(undefined)
-    expect(Robot.placeRobot(1, 10, "NORTH")).toBe(undefined)
+    const robot = new Robot();
+    robot.place(-1, 0, "NORTH");
+    checkRobotValues(robot, false);
+    
+    robot.place(1, -3, "EAST");
+    checkRobotValues(robot, false);
+    
+    robot.place(5, 0, "NORTH");
+    checkRobotValues(robot, false);
+    
+    robot.place(1, 10, "NORTH");
+    checkRobotValues(robot, false);
 });
 
 test("can place robot in valid position", () => {
-    const robotN = Robot.placeRobot(2, 3, "NORTH");
-    expect(robotN.x).toBe(2);
-    expect(robotN.y).toBe(3);
-    expect(robotN.facing).toBe(Direction.North);
+    const robotN = new Robot();
+    robotN.place(2, 3, "NORTH");
+    checkRobotValues(robotN, true, 2, 3, Direction.NORTH);
 
-    const robotE = Robot.placeRobot(0, 4, "EAST");
-    expect(robotE.x).toBe(0);
-    expect(robotE.y).toBe(4);
-    expect(robotE.facing).toBe(Direction.EAST);
+    const robotE = new Robot();
+    robotE.place(0, 4, "EAST");
+    checkRobotValues(robotE, true, 0, 4, Direction.EAST);
 
-    const robotS = Robot.placeRobot(4, 4, "SOUTH");
-    expect(robotS.x).toBe(4);
-    expect(robotS.y).toBe(4);
-    expect(robotS.facing).toBe(Direction.SOUTH);
+    const robotS = new Robot();
+    robotS.place(4, 4, "SOUTH");
+    checkRobotValues(robotS, true, 4, 4, Direction.SOUTH);
 
-    const robotW = Robot.placeRobot(3, 2, "WEST");
-    expect(robotW.x).toBe(3);
-    expect(robotW.y).toBe(2);
-    expect(robotW.facing).toBe(Direction.WEST);
-})
+    const robotW = new Robot();
+    robotW.place(3, 2, "WEST");
+    checkRobotValues(robotW, true, 3, 2, Direction.WEST);
+});
 
 test("cannot move robot outside bounds", () => {
-    // expect(new Robot(0,0,"NORTH") === new Robot(0,0,"NORTH")).toBeTruthy()
+    for (var x = 0; x < 5; x++) {
+        const robotN = new Robot();
+        robotN.place(x, 4, "NORTH");
+        robotN.move();
+        checkRobotValues(robotN, true, x, 4, Direction.NORTH);
+
+        const robotS = new Robot();
+        robotS.place(x, 0, "SOUTH");
+        robotS.move();
+        checkRobotValues(robotS, true, x, 0, Direction.SOUTH);
+    }
+
+    for (var y = 0; y < 5; y++) {
+        const robotE = new Robot();
+        robotE.place(4, y, "EAST");
+        robotE.move();
+        checkRobotValues(robotE, true, 4, y, Direction.EAST);
+
+        const robotW = new Robot();
+        robotW.place(0, y, "WEST");
+        robotW.move();
+        checkRobotValues(robotW, true, 0, y, Direction.WEST);
+    }
+});
+
+test("can move robot within bounds", () => {
+    for (var x = 0; x < 4; x++) {
+        const robotE = new Robot();
+        robotE.place(x, 2, "EAST");
+        robotE.move();
+        checkRobotValues(robotE, true, x + 1, 2, Direction.EAST);
+    }
+
+    for (var x = 1; x < 5; x++) {
+        const robotW = new Robot();
+        robotW.place(x, 4, "WEST");
+        robotW.move();
+        checkRobotValues(robotW, true, x - 1, 4, Direction.WEST);
+    }
+
+    for (var y = 0; y < 4; y++) {
+        const robotN = new Robot();
+        robotN.place(1, y, "NORTH");
+        robotN.move();
+        checkRobotValues(robotN, true, 1, y + 1, Direction.NORTH);
+    }
+
+    for (var y = 1; y < 5; y++) {
+        const robotS = new Robot();
+        robotS.place(3, y, "SOUTH");
+        robotS.move();
+        checkRobotValues(robotS, true, 3, y - 1, Direction.SOUTH);
+    }
+});
+
+test("can rotate robot to the right", () => {
+    const robotN = new Robot();
+    robotN.place(2, 2, "NORTH");
+    robotN.rotateRight();
+    checkRobotValues(robotN, true, 2, 2, Direction.EAST);
+
+    const robotE = new Robot();
+    robotE.place(3, 4, "EAST");
+    robotE.rotateRight();
+    checkRobotValues(robotE, true, 3, 4, Direction.SOUTH);
+
+    const robotS = new Robot();
+    robotS.place(1, 2, "SOUTH");
+    robotS.rotateRight();
+    checkRobotValues(robotS, true, 1, 2, Direction.WEST);
+
+    const robotW = new Robot();
+    robotW.place(4, 2, "WEST");
+    robotW.rotateRight();
+    checkRobotValues(robotW, true, 4, 2, Direction.NORTH);
+    robotW.rotateRight();
+    checkRobotValues(robotW, true, 4, 2, Direction.EAST);
+});
+
+test("can rotate robot to the left", () => {
+    const robotN = new Robot();
+    robotN.place(0, 0, "NORTH");
+    robotN.rotateLeft();
+    checkRobotValues(robotN, true, 0, 0, Direction.WEST);
+
+    const robotW = new Robot();
+    robotW.place(1, 4, "WEST");
+    robotW.rotateLeft();
+    checkRobotValues(robotW, true, 1, 4, Direction.SOUTH);
+
+    const robotS = new Robot();
+    robotS.place(3, 3, "SOUTH");
+    robotS.rotateLeft();
+    checkRobotValues(robotS, true, 3, 3, Direction.EAST);
+
+    const robotE = new Robot();
+    robotE.place(4, 2, "EAST");
+    robotE.rotateLeft();
+    checkRobotValues(robotE, true, 4, 2, Direction.NORTH);
+    robotE.rotateLeft();
+    checkRobotValues(robotE, true, 4, 2, Direction.WEST);
+});
+
+test("can report robot state", () => {
+    const robotN = new Robot();
+    robotN.place(0, 1, "NORTH");
+    expect(robotN.report()).toBe("0,1,NORTH");
+
+    const robotE = new Robot();
+    robotE.place(2, 4, "EAST");
+    expect(robotE.report()).toBe("2,4,EAST");
+
+    const robotS = new Robot();
+    robotS.place(0, 0, "SOUTH");
+    expect(robotS.report()).toBe("0,0,SOUTH");
+    
+    const robotW = new Robot();
+    robotW.place(3, 1, "WEST");
+    expect(robotW.report()).toBe("3,1,WEST");
+});
+
+test("move throws an error when robot not placed", () => {
+    const robot = new Robot();
+    expect(() => robot.move()).toThrow();
+});
+
+test("ignores move command when not placed", () => {
+    const robot = new Robot();
+    robot.followCommand("MOVE");
+    // The robot's location values should all still be undefined
+    checkRobotValues(robot, false);
+});
+
+test("follows move command after being placed", () => {
+    for (var x = 0; x < 4; x++) {
+        const robotE = new Robot();
+        robotE.place(x, 2, "EAST");
+        robotE.followCommand("MOVE");
+        checkRobotValues(robotE, true, x + 1, 2, Direction.EAST);
+    }
+
+    for (var x = 1; x < 5; x++) {
+        const robotW = new Robot();
+        robotW.place(x, 4, "WEST");
+        robotW.followCommand("MOVE");
+        checkRobotValues(robotW, true, x - 1, 4, Direction.WEST);
+    }
+
+    for (var y = 0; y < 4; y++) {
+        const robotN = new Robot();
+        robotN.place(1, y, "NORTH");
+        robotN.followCommand("MOVE");
+        checkRobotValues(robotN, true, 1, y + 1, Direction.NORTH);
+    }
+
+    for (var y = 1; y < 5; y++) {
+        const robotS = new Robot();
+        robotS.place(3, y, "SOUTH");
+        robotS.followCommand("MOVE");
+        checkRobotValues(robotS, true, 3, y - 1, Direction.SOUTH);
+    }
+});
+
+test("rotate right throws an error when robot not placed", () => {
+    const robot = new Robot();
+    expect(() => robot.rotateRight()).toThrow();
+});
+
+test("ignores rotate right command when not placed", () => {
+    const robot = new Robot();
+    robot.followCommand("RIGHT");
+    // The robot's location values should all still be undefined
+    checkRobotValues(robot, false);
+});
+
+test("follows rotate right command after being placed", () => {
+    const robotN = new Robot();
+    robotN.place(2, 2, "NORTH");
+    robotN.followCommand("RIGHT");
+    checkRobotValues(robotN, true, 2, 2, Direction.EAST);
+
+    const robotE = new Robot();
+    robotE.place(3, 4, "EAST");
+    robotE.followCommand("RIGHT");
+    checkRobotValues(robotE, true, 3, 4, Direction.SOUTH);
+
+    const robotS = new Robot();
+    robotS.place(1, 2, "SOUTH");
+    robotS.followCommand("RIGHT");
+    checkRobotValues(robotS, true, 1, 2, Direction.WEST);
+
+    const robotW = new Robot();
+    robotW.place(4, 2, "WEST");
+    robotW.followCommand("RIGHT");
+    checkRobotValues(robotW, true, 4, 2, Direction.NORTH);
+    robotW.followCommand("RIGHT");
+    checkRobotValues(robotW, true, 4, 2, Direction.EAST);
+});
+
+test("rotate left throws an error when robot not placed", () => {
+    const robot = new Robot();
+    expect(() => robot.rotateLeft()).toThrow();
+});
+
+test("ignores rotate left command when not placed", () => {
+    const robot = new Robot();
+    robot.followCommand("LEFT");
+    // The robot's location values should all still be undefined
+    checkRobotValues(robot, false);
+});
+
+test("follows rotate left command after being placed", () => {
+    const robotN = new Robot();
+    robotN.place(0, 0, "NORTH");
+    robotN.followCommand("LEFT");
+    checkRobotValues(robotN, true, 0, 0, Direction.WEST);
+
+    const robotW = new Robot();
+    robotW.place(1, 4, "WEST");
+    robotW.followCommand("LEFT");
+    checkRobotValues(robotW, true, 1, 4, Direction.SOUTH);
+
+    const robotS = new Robot();
+    robotS.place(3, 3, "SOUTH");
+    robotS.followCommand("LEFT");
+    checkRobotValues(robotS, true, 3, 3, Direction.EAST);
+
+    const robotE = new Robot();
+    robotE.place(4, 2, "EAST");
+    robotE.followCommand("LEFT");
+    checkRobotValues(robotE, true, 4, 2, Direction.NORTH);
+    robotE.followCommand("LEFT");
+    checkRobotValues(robotE, true, 4, 2, Direction.WEST);
+});
+
+test("report throws an error when robot not placed", () => {
+    const robot = new Robot();
+    expect(() => robot.report()).toThrow();
+});
+
+test("ignores report command when not placed", () => {
+    const robot = new Robot();
+    robot.followCommand("REPORT");
+    // The robot's location values should all still be undefined
+    checkRobotValues(robot, false);
+});
+
+test("follows report command after being placed", () => {
+    const robotN = new Robot();
+    robotN.place(0, 1, "NORTH");
+    const resultN = robotN.followCommand("REPORT");
+    expect(resultN.report).toBe("0,1,NORTH");
+
+    const robotE = new Robot();
+    robotE.place(2, 4, "EAST");
+    const resultE = robotE.followCommand("REPORT");
+    expect(resultE.report).toBe("2,4,EAST");
+
+    const robotS = new Robot();
+    robotS.place(0, 0, "SOUTH");
+    const resultS = robotS.followCommand("REPORT");
+    expect(resultS.report).toBe("0,0,SOUTH");
+    
+    const robotW = new Robot();
+    robotW.place(3, 1, "WEST");
+    const resultW = robotW.followCommand("REPORT");
+    expect(resultW.report).toBe("3,1,WEST");
+});
+
+test("follows place command", () => {
+    const robotN = new Robot();
+    robotN.followCommand("PLACE 2, 3, NORTH");
+    checkRobotValues(robotN, true, 2, 3, Direction.NORTH);
+
+    const robotE = new Robot();
+    robotE.followCommand("PLACE 0,4,EAST");
+    checkRobotValues(robotE, true, 0, 4, Direction.EAST);
+
+    const robotS = new Robot();
+    robotS.followCommand("PLACE 4,4,SOUTH");
+    checkRobotValues(robotS, true, 4, 4, Direction.SOUTH);
+
+    const robotW = new Robot();
+    robotW.followCommand("PLACE 3,2,WEST");
+    checkRobotValues(robotW, true, 3, 2, Direction.WEST);
+})
+
+test("returns an error on an unknown command", () => {
+    const robotN = new Robot();
+    robotN.place(0, 1, "NORTH");
+    const result = robotN.followCommand("REPOR");
+    expect(result.error).toBe("Unknown command");
+});
+
+test("returns an error on a malformed command", () => {
+    const robotN = new Robot();
+    robotN.place(0, 1, "NORTH");
+    const result = robotN.followCommand("LEFT 5");
+    expect(result.error).toBe("Malformed command");
+});
+
+test("returns an error with missing place arguments", () => {
+    const robot = new Robot();
+    const results = robot.followCommand("PLACE 3");
+    expect(results.error).toBe("Three parameters required when placing");
+});
+
+
+
+
+
+// ===========================
+// Test with provided examples
+// ===========================
+
+test("passes example a", () => {
+    const robot = new Robot();
+
+    robot.followCommand("PLACE 0,0,NORTH");
+    robot.followCommand("MOVE");
+    const result = robot.followCommand("REPORT");
+    expect(result.report).toBe("0,1,NORTH");
+});
+
+test("passes example b", () => {
+    const robot = new Robot();
+    robot.followCommand("PLACE 0,0,NORTH");
+    robot.followCommand("LEFT");
+    const result = robot.followCommand("REPORT");
+    expect(result.report).toBe("0,0,WEST");
+});
+
+test("passes example c", () => {
+    const robot = new Robot();
+    robot.followCommand("PLACE 1,2,EAST");
+    robot.followCommand("MOVE");
+    robot.followCommand("MOVE");
+    robot.followCommand("LEFT");
+    robot.followCommand("MOVE");
+    const result = robot.followCommand("REPORT");
+    expect(result.report).toBe("3,3,NORTH")
+});
+
+// ===========
+// Extra tests
+// ===========
+
+test("only processes commands after first place", () => {
+    const robot = new Robot();
+    robot.followCommand("LEFT");
+    robot.followCommand("LEFT");
+    robot.followCommand("RIGHT");
+    robot.followCommand("LEFT");
+    robot.followCommand("MOVE");
+    const result1 = robot.followCommand("REPORT");
+    checkRobotValues(robot, false);
+    expect(result1.error).toBe(undefined);
+    expect(result1.report).toBe(undefined);
+
+    robot.followCommand("PLACE 0,2,NORTH");
+    robot.followCommand("MOVE");
+    robot.followCommand("MOVE");
+    robot.followCommand("RIGHT");
+    robot.followCommand("MOVE");
+    robot.followCommand("MOVE");
+    robot.followCommand("LEFT");
+    const result2 = robot.followCommand("REPORT");
+    expect(result2.report).toBe("2,4,NORTH");
+});
+
+test("ignores commands that would take it off the table", () => {
+    const robot = new Robot();
+    robot.followCommand("LEFT");
+    robot.followCommand("RIGHT");
+    robot.followCommand("PLACE 3,1,EAST");
+    robot.followCommand("MOVE");
+    robot.followCommand("MOVE");
+    const result1 = robot.followCommand("REPORT");
+    expect(result1.report).toBe("4,1,EAST");
+    
+    robot.followCommand("RIGHT");
+    robot.followCommand("MOVE");
+    const result2 = robot.followCommand("REPORT");
+    expect(result2.report).toBe("4,0,SOUTH");
+    
+    robot.followCommand("MOVE");
+    const result3 = robot.followCommand("REPORT");
+    expect(result3.report).toBe("4,0,SOUTH");
+    
+    robot.followCommand("LEFT");
+    robot.followCommand("MOVE");
+    robot.followCommand("LEFT");
+    const result4 = robot.followCommand("REPORT");
+    expect(result4.report).toBe("4,0,NORTH");
+});
+
+test("handles multiple places", () => {
+    const robot = new Robot();
+    robot.followCommand("PLACE 3,2,SOUTH");
+    const result1 = robot.followCommand("REPORT");
+    expect(result1.report).toBe("3,2,SOUTH");
+
+    robot.followCommand("PLACE 4,4, WEST");
+    const result2 = robot.followCommand("REPORT");
+    expect(result2.report).toBe("4,4,WEST");
+
+    robot.followCommand("MOVE");
+    robot.followCommand("MOVE");
+    robot.followCommand("MOVE");
+    robot.followCommand("LEFT");
+    robot.followCommand("MOVE");
+    const result3 = robot.followCommand("REPORT");
+    expect(result3.report).toBe("1,3,SOUTH");
+    
+    robot.followCommand("PLACE 3,2,EAST");
+    robot.followCommand("MOVE");
+    robot.followCommand("MOVE");
+    robot.followCommand("RIGHT");
+    robot.followCommand("MOVE");
+    robot.followCommand("MOVE");
+    robot.followCommand("RIGHT");
+    const result4 = robot.followCommand("REPORT");
+    expect(result4.report).toBe("4,0,WEST");
 })
